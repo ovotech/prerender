9722c0dc28c64006c622060802aba15212d3fab8
Stuart Izon <stuart.izon@ovoenergy.com>
2015-05-27 11:21:16 +0100

    QS-234 Added grunt dependencies and initial version of grunt file

---
+++ gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function (grunt) {
+    var pkg = grunt.file.readJSON('package.json');
+
+    grunt.config.merge({
+        pkg: pkg,
+        compress: {
+            main: {
+                options: {
+                    archive: 'archives/<%= pkg.version %>.zip'
+                },
+                files: [
+                    { expand: true, src: ['server.js'] }
+                ]
+            }
+        },
+        aws_s3: {
+            options: {
+                region: 'eu-west-1',
+                uploadConcurrency: 5,
+                downloadConcurrency: 5
+            },
+            publish: {
+                options: {
+                    bucket: 'ovo-bower-repo',
+                    differential: true
+                },
+                files: [
+                    { expand: true, cwd: 'archives/', src: ['<%= pkg.version %>.zip'], dest: '<%= pkg.name %>/'}
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-aws-s3');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    
+    grunt.registerTask('publish', ['compress', 'aws_s3:publish']);
+};
