7544f16a9900de3cdcc5cd1111f266e81732fb07
Stuart Izon <stuart.izon@ovoenergy.com>
2015-06-12 13:07:51 +0100

    QS-234 Added logic to handle phantom arguments (e.g. proxy)

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
+var args;
+if (process.env.PHANTOM_ARGS != null)
+    args = process.env.PHANTOM_ARGS.split(" ");
+
 var server = prerender({
     workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
     iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
     phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
-    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
+    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT,
+    phantomArguments: args
 });
 
 
