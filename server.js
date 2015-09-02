af49afded94ed428926fbc79c61f193116e577c9
danielAparicio <daniel.aparicio.urbina@gmail.com>
2015-09-02 16:19:17 +0100

    another approach to fix the ssl problem keeping the uat proxy

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-//commenting the args and the phantomArguments to use the ones in /lib/service.js
-//var args;
-//if (process.env.PHANTOM_ARGS != null)
-//    args = process.env.PHANTOM_ARGS.split(" ");
+commenting the args and the phantomArguments to use the ones in /lib/service.js
+var args;
+if (process.env.PHANTOM_ARGS != null)
+    args = process.env.PHANTOM_ARGS.split(" ");
 
 var server = prerender({
     workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
     iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
     phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
     messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT,
-//  phantomArguments: args
+    phantomArguments: args
 });
 
 
